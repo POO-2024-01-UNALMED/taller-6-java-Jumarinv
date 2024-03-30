96bddf2301a6569ab4518114dff0f2443f3d8ee0
Jumarinv <jumarinv@unal.edu.co>
2024-03-30 10:39:15 -0500

    Taller6Java1.1
    -Metodo setCantidadVehiculos agregado

--- src/test/java/vehiculos/Vehiculo.java
+++ src/test/java/vehiculos/Vehiculo.java
@@ -73,5 +73,7 @@ public class Vehiculo {
 	
 	
 	public static int getCantidadVehiculos () {return(cantidadVehiculos);}
+	
+	public static void setCantidadVehiculos (int num) {cantidadVehiculos = num;}
 
 }
